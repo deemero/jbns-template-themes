0a3759c7d83e8267016a710c6ea35d658b82d374
deemero <neroalex93@gmail.com>
2023-09-06 14:17:57 +0800

    change color nama  and logo size

--- mainframes/navbar.php
+++ mainframes/navbar.php
@@ -8,7 +8,7 @@
         </button>
         <div class="collapse navbar-collapse justify-content-between px-0 px-lg-3" id="navbarCollapse">
             <div class="navbar-nav mr-auto py-0">
-                <a href="index.php" class="nav-item nav-link active">Laman Utama</a>
+                <a href="index.php" class="nav-item nav-link ">Laman Utama</a>
                 <?php
 // Fetch main menu items
 $query = "SELECT * FROM menu_main WHERE website_id = '$website_id' AND page = '' AND trash != '1' GROUP BY menu ORDER BY main_order ASC";
